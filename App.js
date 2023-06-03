06b4d8e952c0f5fc4e0615f4aa5a47b25c8da124
SURAJ RAJJAK  MANERI <98664760+2063261612003@users.noreply.github.com>
2023-06-04 01:14:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CovidRegistrationForm from './Component/CovidRegistrationForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CovidRegistrationForm/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
